f146d49bde0270e762a671d1dd95d63c903f1f84
markus.ziegler@hpe.com <markus.ziegler@hpe.com>
2024-09-24 16:45:33 +0200

    Update

--- libraries/dispatch_production_POC.ts
+++ libraries/dispatch_production_POC.ts
@@ -9,9 +9,9 @@ function dispatch(event : any) : string {
     // Dispatch poc SIP Invites & poc SIP Options
     try {
 
-        if ( event.SIP.message.method[0].match(/^OPTIONS$/) && event.SIP["R-URI"].address.uri.user != null) {
+        if ( event.SIP.message.method[0].match(/^OPTIONS$/) && event.SIP["R-URI"].address.uri != null) {
 
-            if (event.SIP["R-URI"].address.uri.user.match(/^10/)) {
+            if (event.SIP["R-URI"].address.value.match(/^sip:10/)) {
 
                 return "production:poc:poc_main";
                 
